332c607b3a01ab70fa29359efc726d56e2ab73a5
progIfe <ifeoluwasamson90@gmail.com>
2023-06-28 19:01:51 +0100

    User Display Screen CSS

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.lobby, name='lobby'),
+    path('room/', views.room, name='room'),
 ]
--- base/views.py
+++ base/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def lobby(request):
     return render(request, 'screens/lobby.html')
+
+def room(request):
+    return render(request, 'screens/room.html')
+
